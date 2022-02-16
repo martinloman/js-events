5517c71c472d8bc6519a11ec8837d33f913d10ab
martinloman <martin.loman@gmail.com>
2022-02-16 10:12:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+let btn = document.getElementById("btn")
+btn.onclick = function (event) {
+  console.log("Klickade på knappen.")
+}
+btn.focus = function (event) {
+  console.log("Focus på knappen.")
+}
+
+let text = document.getElementById("txtInput")
+
+text.onclick = function (event) {
+  console.log("Klickade i textfältet.")
+}
+
+text.onfocus = function (event) {
+  console.log("Focus i textfältet.")
+}
+
+text.onkeydown = function (event) {
+  console.log(`Du tryckte på ${event.key}`)
+
+  // Om man vill kan man lyssna efter specifika tangenttryck och agera på dem.
+  // Här är kod som förhindrar att sidan laddas om (formuläret skickas) om man trycker på Enter-tangenten.
+  if (event.key === "Enter") {
+    event.preventDefault()
+  }
+}
+
+document.body.onclick = function (event) {
+  console.log("Klickade i body.")
+}
+
+/*
+  Argumentet event är event-objekt med en massa information om eventet.
+  Den informationen kan man använda i sin funktion.
+*/
+document.onmousemove = function (event) {
+  //console.log(`Musen flyttas över ${event.currentTarget}`)
+  //console.log(`Musen flyttas på koordinat (${event.pageX}, ${event.pageY})`)
+}
+
+let div = document.getElementById("mouseOverDemo")
+div.onmousemove = function (event) {
+  //console.log(`Musen flyttas över ${event.currentTarget}`)
+  console.log(`Musen flyttas på sid-koordinat (${event.pageX}, ${event.pageY})`)
+  console.log(
+    `Musen flyttas på koordinat (${event.offsetX}, ${event.offsetY}) i div:en`
+  )
+
+  console.log(
+    `Musen flyttas med hastighet (${event.movementX}, ${event.movementY})`
+  )
+
+  // Om du tar bort kommenteringen på raden nedan kommer mousemove-eventet INTE
+  // gå vidare till eventehanteraren för mousemove för dokumentet.
+  //event.stopImmediatePropagation()
+}
+
+let canvas = document.getElementById("myCanvas")
+let ctx = canvas.getContext("2d")
+
+canvas.onmousemove = function (event) {
+  /*
+  ctx.beginPath()
+  let coordX = event.offsetX
+  let coordY = event.offsetY
+  console.log(coordX, coordY)
+  ctx.lineWidth = 5
+
+  // Sets the end of the lines drawn
+  // to a round shape.
+  ctx.lineCap = "round"
+
+  ctx.strokeStyle = "green"
+
+  // The cursor to start drawing
+  // moves to this coordinate
+  ctx.moveTo(coordX, coordY)
+
+  // A line is traced from start
+  // coordinate to this coordinate
+  ctx.lineTo(coordX, coordY)
+
+  // Draws the line.
+  ctx.stroke()
+  */
+}
---
+++ keypress-demo.js
@@ -0,0 +1,29 @@
+// Så här namnges konstanter i JavaScript.
+const CAR_STEP_SIZE = 10
+
+let span = document.getElementById("result")
+let car = document.getElementById("car")
+
+document.body.onkeydown = function (event) {
+  span.innerText = event.key
+
+  if (event.key === "ArrowDown") {
+    // Flytta bilen nedåt i sidan. I positiv riktning i Y-led i sidans koordinatsystem.
+    moveY(car, CAR_STEP_SIZE)
+  }
+}
+
+function moveY(element, distance) {
+  // top är t.ex. 100px
+  let top = getComputedStyle(element).top
+
+  // ta bort "px" från slutet av top, genom att ersätta det med en tom sträng
+  top = top.replace("px", "")
+
+  // top är en sträng, för att kunna lägga till distance måste vi konvertera den till ett tal.
+  // för att få ett heltal så avrundar vi top.
+  let newTop = Math.round(Number(top)) + distance
+
+  //när man ska sätta nytt värde på top ska det vara en sträng med enhet.
+  element.style.top = newTop + "px"
+}
